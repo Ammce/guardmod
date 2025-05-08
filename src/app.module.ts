0b9f946e24c70f9ff780bcdc133cedeae12f1e04
Amel Muminovic <amcenp@gmail.com>
2025-05-07 21:40:20 -0400

    feat: added moderator module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ModeratorModule } from './modules/moderator/moderator.module';
 
 @Module({
-  imports: [],
+  imports: [ModeratorModule],
   controllers: [AppController],
   providers: [AppService],
 })
