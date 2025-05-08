b4ae5d3176613173411df02dcba576a2a8a84a89
Amel Muminovic <amcenp@gmail.com>
2025-05-08 17:19:53 -0400

    feat: added a comment for content moderation

--- src/modules/moderator/moderator.controller.ts
+++ src/modules/moderator/moderator.controller.ts
@@ -1,9 +1,34 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { ModeratorService } from './moderator.service';
+import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+// TODO - Finish up documentation for all endpoints
+@ApiTags('Moderator')
 @Controller('moderator')
 export class ModeratorController {
   constructor(private readonly moderatorService: ModeratorService) {}
+
+  @ApiOperation({ summary: 'Moderate a comment' })
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        comment: { type: 'string' },
+      },
+    },
+  })
+
+  // TODO - Finish up Schema for response
+  @ApiResponse({
+    status: 200,
+    description: 'The comment has been moderated',
+    schema: {
+      type: 'object',
+      properties: {
+        comment: { type: 'string' },
+      },
+    },
+  })
   @Post('comments')
   moderateComment(@Body() { comment }: { comment: string }) {
     console.log('Comment is ', comment);
